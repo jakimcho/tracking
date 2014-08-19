736115f4bf133c7989a9ceca8247bc423426121a
jakim <jakimcho@gmail.com>
2014-08-19 21:46:30 +0300

    First Commit

---
+++ src/main/java/com/jakim/tracking/entities/Person.java
@@ -0,0 +1,36 @@
+package com.jakim.tracking.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(name="name")
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/com/jakim/tracking/services/PersonService.java
@@ -0,0 +1,45 @@
+package com.jakim.tracking.services;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jakim.tracking.entities.Person;
+
+@Service
+public class PersonService {
+
+	// An EntityManager will be automatically injected from EntityManagerFactory
+	// setup on
+	// spring-context.xml
+
+	@PersistenceContext
+	private EntityManager em;
+
+	// Since we've setup <tx:annotation-config> and transaction manager on
+	// spring-context.xml,
+	// any bean method annotated with @Transactional will cause Spring to
+	// magically call
+	// begin() and commit() at the start/end of the method. If exception occurs
+	// it will also
+	// call rollback()
+
+	@Transactional
+	public List<Person> getAll() {
+		List<Person> result = em.createQuery("SELECT p FROM Person p",
+				Person.class).getResultList();
+		return result;
+	}
+
+	@Transactional
+	public void add(Person p) {
+		System.out.println("Enter in PersonService.add");
+
+		em.persist(p);
+		
+		System.out.println("persist " + p.getName());
+	}
+}
